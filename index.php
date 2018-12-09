2f732cceb6488a00947b0c73d41515a9414939da
yadhun <yadhunandan64@gmail.com>
2018-12-09 17:58:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>Vipra Community</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="OneTech shop project">
+<meta name="description" content="Communit project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
 <link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
